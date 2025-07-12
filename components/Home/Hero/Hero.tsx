b13e7a253c999d62186faca50e78a807cec9d00b
Silaenn <deokeldisilaen@gmail.com>
2025-07-13 00:23:46 +0700

    add hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,3 +1,5 @@
+import SearchBox from "@/components/Helper/SearchBox";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
@@ -21,7 +23,19 @@ const Hero = () => {
             <h1 className="text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold uppercase">
               Lets Enjoy The Nature
             </h1>
+            <p className="md:text-base text-center text-lg text-white font-normal [word-spacing:5px]">
+              Get the best prices on 2,000,000+ properties,worldwide
+            </p>
           </div>
+          {/* Search Box */}
+          <SearchBox />
+          <Link
+            href="#"
+            className="rounded px-14 md:px-28 -mt-4 py-2.5 overflow-hidden group bg-rose-600 relative hover:bg-gradient-to-r hover:form-red-500 hover:to-red-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-red-400 transition-all ease-out duration-300"
+          >
+            <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+            <span className="relative font-bold">Search</span>
+          </Link>
         </div>
       </div>
     </div>
