b6bba562ccf70f95fc7d64e4052523109e54c9b9
Silaenn <deokeldisilaen@gmail.com>
2025-07-13 23:38:37 +0700

    add footer and scroptotop

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
+import ScrolToTop from "@/components/Helper/ScrolToTop";
 
 const font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -32,6 +34,8 @@ export default function RootLayout({
       <body className={`${font.className} antialiased`}>
         <ResponsiveNav />
         {children}
+        <Footer />
+        <ScrolToTop />
       </body>
     </html>
   );
