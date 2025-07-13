5b6ee657fbfedd061a662b0fc5d6cf9706e02bdc
Silaenn <deokeldisilaen@gmail.com>
2025-07-13 15:18:00 +0700

    add destination

---
+++ components/Home/Hotel/Hotel.tsx
@@ -0,0 +1,13 @@
+import SectionHeading from "@/components/Helper/SectionHeading";
+import React from "react";
+
+const Hotel = () => {
+  return (
+    <div className="pt-20 pb-20">
+      {/* Section Heading */}
+      <SectionHeading heading="Recommended Hotels" />
+    </div>
+  );
+};
+
+export default Hotel;
