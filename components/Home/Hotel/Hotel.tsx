44dab3ca8c25c6cba79442eca622ba7ff134b45a
Silaenn <deokeldisilaen@gmail.com>
2025-07-14 00:18:24 +0700

    add anim

--- components/Home/Hotel/Hotel.tsx
+++ components/Home/Hotel/Hotel.tsx
@@ -10,9 +10,14 @@ const Hotel = () => {
       <SectionHeading heading="Recommended Hotels" />
       <div className="w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-center mt-16">
         {/* HotelCard */}
-        {hotelsData.map((data) => {
+        {hotelsData.map((data, i) => {
           return (
-            <div key={data.id}>
+            <div
+              data-aos="fade-right"
+              data-aos-anchor-placement="top-center"
+              data-aos-delay={`${i * 100}`}
+              key={data.id}
+            >
               <HotelCard hotel={data} />
             </div>
           );
